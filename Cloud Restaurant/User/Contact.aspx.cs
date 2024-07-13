d7a234b0d8d9ebc2906e36518ef78beb8f24685e
Asad <129451320+Asadul09@users.noreply.github.com>
2024-07-13 23:30:18 +0600

    Contacts

--- Cloud Restaurant/User/Contact.aspx.cs
+++ Cloud Restaurant/User/Contact.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,49 @@ namespace Cloud_Restaurant.User
 {
     public partial class Contact : System.Web.UI.Page
     {
+        SqlConnection con;
+        SqlCommand cmd;
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con = new SqlConnection(Connection.GetConnectionString());
+                cmd = new SqlCommand("ContactSp", con);
+                cmd.Parameters.AddWithValue("@Action", "INSERT");
+                cmd.Parameters.AddWithValue("@Name", txtName.Text.Trim());
+                cmd.Parameters.AddWithValue("@Email", txtEmail.Text.Trim());
+                cmd.Parameters.AddWithValue("@Subject", txtSubject.Text.Trim());
+                cmd.Parameters.AddWithValue("@Massage", txtMassage.Text.Trim());
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                cmd.ExecuteNonQuery();
+                lblMsg.Visible = true;
+                lblMsg.Text = "Thanks for reaching out will look into your query!";
+                lblMsg.CssClass = "alert alert-success";
+                Clear();
+            }
+            catch(Exception ex)
+            {
+                Response.Write("<script>alert(' " + ex.Message + " ');</script>");
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void Clear()
+        {
+            txtName.Text = string.Empty;
+            txtEmail.Text = string.Empty;
+            txtSubject.Text = string.Empty;
+            txtMassage.Text = string.Empty;
         }
     }
 }
